c869634e217aa459224d903512b35dfe46d65e96
guojiao1993 <wang2012jida@163.com>
2018-07-24 21:45:40 +0800

    对中文使用HashMap进行处理

--- src/chapter_5_stringproblem/Problem_01_IsDeformation.java
+++ src/chapter_5_stringproblem/Problem_01_IsDeformation.java
@@ -1,5 +1,8 @@
 package chapter_5_stringproblem;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem_01_IsDeformation {
 
 	public static boolean isDeformation(String str1, String str2) {
@@ -20,11 +23,42 @@ public class Problem_01_IsDeformation {
 		return true;
 	}
 
+	public static boolean isDeformationUsingMap(String str1, String str2) {
+		if (str1 == null || str2 == null || str1.length() != str2.length()) {
+			return false;
+		}
+		Map<String, Integer> map = new HashMap<>();
+		for (int i = 0; i < str1.length(); i++) {
+			String s = str1.substring(i, i + 1);
+			if(map.containsKey(s)) {
+				map.put(s, map.get(s) + 1);
+			} else {
+				map.put(s, 1);
+			}
+		}
+		for (int i = 0; i < str2.length(); i++) {
+			String s = str2.substring(i, i + 1);
+			if(map.containsKey(s)) {
+				Integer num = map.get(s);
+				if(num <= 0) {
+					return false;
+				} else {
+					map.put(s, num - 1);
+				}
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		String A = "abcabcabc";
 		String B = "bcacbaacb";
 		System.out.println(isDeformation(A, B));
-
+		A = "ÄãºÃÄãÄãÄã";
+		B = "ºÃÅ¶ÄãÄãÄã";
+		System.out.println(isDeformationUsingMap(A, B));
 	}
 
 }
